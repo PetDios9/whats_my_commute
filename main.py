51424c21f7b7199d124bc819c8ddb1a46aef9a18
Peter Diosdado <diosdadopeter@gmail.com>
2024-02-28 10:30:29 -0800

    feat: changes message if getting distance fails

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from get_commute import get_commute
 from send_message import send_message
 from dotenv import load_dotenv
+import sys
 import os
 
 EMAIL = os.getenv("EMAIL")
@@ -14,5 +15,8 @@ CARRIER = os.getenv("CARRIER")
 distance, duration = get_commute(API_KEY, START, END)
 
 MESSAGE = f"Hello Peter. The time it will take you to work is {duration}"
+
+if distance is None: 
+    MESSAGE = "There was an error! Fix it Peter!"
 
 send_message(PHONE_NUMBER, CARRIER, MESSAGE)
